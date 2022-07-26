ae7ac2e1cd60b42d4f73395887ede77003ca870f
LukSiman <simanavicius.lukas@gmail.com>
2022-07-25 18:28:30 -0700

    reformatting

--- proj1/deque/LinkedListDequeTest.java
+++ proj1/deque/LinkedListDequeTest.java
@@ -15,8 +15,6 @@ public class LinkedListDequeTest {
      * && is the "and" operation. */
     public void addIsEmptySizeTest() {
 
-//        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
@@ -35,7 +33,6 @@ public class LinkedListDequeTest {
 
 		System.out.println("Printing out deque: ");
 		lld1.printDeque();
-
     }
 
     @Test
