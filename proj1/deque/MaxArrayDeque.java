3e82823139495d0f2c1e6848ac555eb47f9cede6
LukSiman <simanavicius.lukas@gmail.com>
2022-08-21 17:08:17 -0700

    implementing MaxArrayDeque class

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,51 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque implements Comparable<T> {
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        comparator = c;
+    }
+
+    public T max() {
+        if (this.size() == 0) {
+            return null;
+        }
+
+        int maxIndex = 0;
+
+        for(int i = 0; i < this.size(); i++){
+            int compareValue = comparator.compare((T)this.get(i), (T)this.get(maxIndex));
+            if(compareValue > 0){
+                maxIndex = i;
+            }
+        }
+
+        return (T) this.get(maxIndex);
+    }
+
+
+    public T max(Comparator<T> c) {
+        if (this.size() == 0) {
+            return null;
+        }
+
+        int maxIndex = 0;
+
+        for(int i = 0; i < this.size(); i++){
+            int compareValue = c.compare((T)this.get(i), (T)this.get(maxIndex));
+            if(compareValue > 0){
+                maxIndex = i;
+            }
+        }
+
+        return (T) this.get(maxIndex);
+    }
+
+    @Override
+    public int compareTo(T anotherObject) {
+        return 0;
+    }
+}
