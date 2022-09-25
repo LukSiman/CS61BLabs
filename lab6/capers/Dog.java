d22eee461359c78b25f3ef210c9d21b87b11c3df
LukSiman <simanavicius.lukas@gmail.com>
2022-09-25 14:55:39 -0700

    Adding saving and reading to/from files with serialization

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,17 +1,18 @@
 package capers;
 
+import jdk.jshell.execution.Util;
+
 import java.io.File;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Lukas Simanavicius
 */
-public class Dog { // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
     static final File DOG_FOLDER = Utils.join(".capers", "dogs");
-    // TODO (hint: look at the `join` function in Utils)
 
     /** Age of dog. */
     private int age;
@@ -39,8 +40,9 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File dogFile = Utils.join(DOG_FOLDER, name);
+        return Utils.readObject(dogFile, Dog.class);
+//        return null;
     }
 
     /**
@@ -55,8 +57,16 @@ public class Dog { // TODO
     /**
      * Saves a dog to a file for future use.
      */
-    public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+    public void saveDog() throws RuntimeException {
+        // Check if dog already exists
+        Dog dog = fromFile(this.name);
+        if(dog != null){
+            throw new RuntimeException("Dog already exists");
+        }
+
+        // Write the dog object to its file
+        File dogFile = Utils.join(DOG_FOLDER, this.name);
+        Utils.writeObject(dogFile, this);
     }
 
     @Override
