317de50f583aae2fdce0f23080e11ff91ada1d49
Lukas Simanavicius <simanavicius.lukas@gmail.com>
2023-04-08 12:10:14 -0700

    initial changes

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -4,23 +4,49 @@ package gitlet;
 
 import java.util.Date; // TODO: You'll likely use this in this class
 
-/** Represents a gitlet commit object.
+/**
+ * Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ * @author TODO
  */
 public class Commit {
     /**
-     * TODO: add instance variables here.
      *
      * List all instance variables of the Commit class here with a useful
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided one example for `message`.
      */
 
-    /** The message of this Commit. */
+    /**
+     * The message of this Commit.
+     */
     private String message;
 
-    /* TODO: fill in the rest of this class. */
+    //Timestamp of the commit
+    private String timestamp;
+
+    //Parent of the commit
+    private Commit parent;
+
+    public Commit(Commit parent, String message) {
+        this.parent = parent;
+        if(this.parent == null){
+            this.timestamp = "00:00:00 UTC, Thursday, 1 January 1970";
+        }
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public Commit getParent() {
+        return parent;
+    }
 }
--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,19 +1,27 @@
 package gitlet;
 
-/** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+/**
+ * Driver class for Gitlet, a subset of the Git version-control system.
+ *
+ * @author TODO
  */
 public class Main {
 
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+    /**
+     * Usage: java gitlet.Main ARGS, where ARGS contains
+     * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        if (args.length == 0) {
+            Utils.message("Please enter a command.");
+            System.exit(0);
+        }
+
         String firstArg = args[0];
-        switch(firstArg) {
+        switch (firstArg) {
             case "init":
                 // TODO: handle the `init` command
+                Commit initial = new Commit(null, "initial commit");
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
